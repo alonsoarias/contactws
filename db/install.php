6edaac6fdc2c3a767dfcdfa6a1abeb87a8ad6408
alonsoarias <noveno1996@hotmail.com>
2024-05-14 17:23:20 -0500

    Update install.php

--- db/install.php
+++ db/install.php
@@ -1,32 +1,42 @@
 <?php
-// This file is part of Moodle - https://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
-
 /**
- * Code to be executed after the plugin's database scheme has been installed is defined here.
+ * Installation script for auth_contactws.
  *
- * @package     auth_contactws
- * @category    upgrade
- * @copyright   2024 Soporte IngeWeb <soporte@ingeweb.co>
- * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * This file is part of Moodle - http://moodle.org/
+ *
+ * Moodle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Moodle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    auth_contactws
+ * @category   auth
+ * @copyright  2024 Soporte IngeWeb <soporte@ingeweb.co>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 /**
- * Custom code to be run on installing the plugin.
+ * Custom installation function for the auth_contactws plugin.
+ *
+ * This function is called during the plugin installation process.
+ * It performs custom actions like cleaning up password fields in the user table
+ * for users where 'auth' is set to 'contactws'.
+ *
+ * @global stdClass $CFG Global configuration object.
+ * @global moodle_database $DB Global database object.
  */
 function xmldb_auth_contactws_install() {
+    global $CFG, $DB;
 
-    return true;
+    // Remove cached passwords, as they are not needed for this plugin.
+    // It's a security practice to not store passwords in a cache.
+    $DB->set_field('user', 'password', 'not cached', ['auth' => 'contactws']);
 }
