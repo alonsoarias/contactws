466c1f7e13122b600845b1966dfc8320abcd0302
alonsoarias <noveno1996@hotmail.com>
2024-05-14 17:23:30 -0500

    Update version.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'auth_contactws';
 $plugin->release = '0.1.0';
-$plugin->version = 2024051400;
+$plugin->version = 2024051401;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
