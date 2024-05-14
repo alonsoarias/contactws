f7d222d75fc7568161a3d6f05e0c893f42b7fe39
alonsoarias <noveno1996@hotmail.com>
2024-05-14 16:58:42 -0500

    Update install.php

--- db/install.php
+++ db/install.php
@@ -1,11 +1,32 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
+/**
+ * Code to be executed after the plugin's database scheme has been installed is defined here.
+ *
+ * @package     auth_contactws
+ * @category    upgrade
+ * @copyright   2024 Soporte IngeWeb <soporte@ingeweb.co>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Custom code to be run on installing the plugin.
+ */
 function xmldb_auth_contactws_install() {
-    global $CFG, $DB;
 
-    // upgrade from 1.9.x, introducing version.php
-
-    // remove cached passwords, we do not need them for this plugin
-    $DB->set_field('user', 'password', 'not cached', array('auth'=>'contactws'));
-
+    return true;
 }
