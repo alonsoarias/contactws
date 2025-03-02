0e6fcb3025a61aa0136fe35300a3129f50a898c1
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-03-02 14:32:27 -0500

    Update user_field_mapping.php

--- classes/user_field_mapping.php
+++ classes/user_field_mapping.php
@@ -22,7 +22,7 @@ use core_text;
  * User field mapping class.
  *
  * @package    auth_contactws
- * @copyright  2024 Your Name
+ * @copyright  2025 Pedro Arias <soporte@ingeweb.co>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class user_field_mapping {
